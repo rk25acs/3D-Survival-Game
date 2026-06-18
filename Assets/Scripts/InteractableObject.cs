25ed70fa942f1581788c04007c4fb2abd135f993
Rahul Kolupoti <rahulkolupoti@gmail.com>
2026-06-18 17:40:08 +0100

    Part-4 : Picking Up Items

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -4,10 +4,38 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour
 {
+    public bool playerInRange;
     public string ItemName;
 
     public string GetItemName()
     {
         return ItemName;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && playerInRange && SelectionManager.Instance.onTarget)
+        {
+            Debug.Log(ItemName + "added to Inventory!");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+
+            playerInRange = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")){
+
+            playerInRange= false;
+
+        }
     }
 }
